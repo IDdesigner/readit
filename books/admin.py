f6cc3160ffddbc076c4c86a8a89c0149a75a373f
Rye Crowen <ryecrowendesign@mac.com>
2016-02-28 11:55:25 -0800

    fix typo on date_reviewed

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Author, Book
 
+class BookAdmin(admin.ModelAdmin):
+	fieldsets = [
+		("Book Details", {"fields": ["title", "authors"]}),
+		("Review", {"fields": ["is_favourite", "review", "date_reviewed"]}),
+	]
+
 # Register your models here.
 admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
--- books/models.py
+++ books/models.py
@@ -7,7 +7,7 @@ class Book(models.Model):
 	title = models.CharField(max_length=150)
 	authors = models.ManyToManyField("Author", related_name="books")
 	review = models.TextField(blank=True, null=True)
-	data_reviewed = models.DateTimeField(blank=True, null=True)
+	date_reviewed = models.DateTimeField(blank=True, null=True)
 	is_favourite = models.BooleanField(default=False, verbose_name="Favourite?")
 
 	def __str__(self):
